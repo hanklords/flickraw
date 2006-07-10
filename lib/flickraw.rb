59079bba47da07945fea6770ceac2e736d9ab4b2
hanklords <hanklords@f498cc59-3d18-0410-92c7-822e43d00bf0>
2006-07-10 19:22:18 +0000

    Initial import

---
+++ lib/flickraw.rb
@@ -0,0 +1,205 @@
+require 'rexml/document'
+require 'open-uri'
+require 'md5'
+
+module FlickRaw
+  VERSION='0.1'
+
+  # URL of the flickr REST api
+  REST_URL='http://www.flickr.com/services/rest/?'.freeze
+
+  # URL of the flickr auth page
+  AUTH_URL='http://flickr.com/services/auth/?'
+
+  @api_key = '7b124df89b638e545e3165293883ef62'
+
+  # This is a wrapper around the xml response which provides an easy interface.
+  class Xml 
+    # Returns the text content of the response
+    attr_reader :to_s
+
+    # Returns the raw xml of the response
+    attr_reader :to_xml
+
+    def initialize(xml) # :nodoc:
+      @to_s   = xml.texts.join(' ')
+      @to_xml = xml.to_s
+      @list = nil
+
+      xml.attributes.each {|a, v| attribute a, v }
+
+      if xml.name =~ /s\z/
+        elements = REXML::XPath.match( xml, xml.name.sub(/s\z/, ''))
+        unless elements.empty?
+          @list = elements.collect { |e| Xml.new e }
+        end
+      else
+        xml.elements.each {|e| attribute e.name, Xml.new(e) }
+      end
+    end
+
+    def respond_to?(m) # :nodoc:
+      super || @list.respond_to?(m)
+    end
+
+    private
+    def method_missing(sym, *args, &block)
+      if @list.respond_to?( sym)
+        @list.send sym, *args, &block
+      else
+        super
+      end
+    end
+
+    def attribute(sym, value)
+      meta = class << self; self; end
+      meta.class_eval { define_method(sym) { value } }
+    end
+  end
+
+  # This is what you get in response to an API call.
+  class Response < Xml
+    # This is called internally. It builds the response object according to xml response from the server.
+    def initialize(raw_xml)
+      doc = REXML::Document.new raw_xml
+      super doc.root
+      super doc.root.elements[1] if doc.root.elements.size == 1
+    end
+  end
+
+  class FailedResponse < StandardError
+    attr_reader :code
+    alias :msg :message
+    def initialize(msg, code)
+      @code = code
+      super( msg)
+    end
+  end
+
+  class Request
+    def initialize(flickr = nil) # :nodoc:
+      @flickr = flickr
+
+      self.class.flickr_objects.each {|name|
+        klass = self.class.const_get name.capitalize
+        instance_variable_set "@#{name}", klass.new(@flickr)
+      }
+    end
+
+    def self.build_request(req) # :nodoc:
+      method_nesting = req.split '.'
+      raise "'#{@name}' : Method name mismatch" if method_nesting.shift != request_name.split('.').last
+
+      if method_nesting.size > 1
+        name = method_nesting.first
+        class_name = name.capitalize
+        if const_defined? class_name
+          klass = const_get( class_name)
+        else
+          klass = Class.new Request
+          const_set(class_name, klass)
+          attr_reader name
+	  flickr_objects << name
+        end
+
+        klass.build_request method_nesting.join('.')
+      else
+        req = method_nesting.first
+        define_method(req) { |*args|
+          class_req = self.class.request_name
+          @flickr.call class_req + '.' + req, *args
+        }
+        flickr_methods << req
+      end
+    end
+
+    # List of the flickr subobjects of this object
+    def self.flickr_objects
+      @flickr_objects ||= []
+    end
+
+    # List of the flickr methods of this object
+    def self.flickr_methods
+      @flickr_methods ||= []
+    end
+
+    # Returns the prefix of the request corresponding to this class.
+    def self.request_name
+      class_req = name.downcase.gsub( /::/, '.')
+      class_req.sub( /[^\.]+\./, '')              # Removes RawFlickr at the beginning
+    end
+  end
+
+  # Root class of the flickr api hierarchy.
+  class Flickr < Request
+    def initialize # :nodoc:
+      super self
+      @token = nil
+    end
+
+    # This is the central method. It does the actual request to the flickr server.
+    #
+    # Raises FailedResponse if the response status is _failed_.
+    def call(req, args={})
+      url = build_call_url(req, args)
+      res = Response.new open(url, 'User-Agent' => "Flickraw/#{VERSION}")
+      raise FailedResponse.new( res.msg, res.code) if res.stat == 'fail'
+      lookup_token( req, res)
+      res
+    end
+
+    private
+    def build_call_url(req, args={})
+      full_args = {:api_key => FlickRaw.api_key, :method => req}
+      full_args[:auth_token] = @token if @token
+      args.each {|k, v| full_args[k.to_sym] = v }
+      full_args[:api_sig] = FlickRaw.api_sig(full_args)  if FlickRaw.shared_secret
+
+      url = REST_URL + full_args.collect { |a, v| "#{a}=#{v}" }.join('&')
+    end
+
+    def lookup_token(req, res)
+      token_reqs = ['flickr.auth.getToken', 'flickr.auth.getFullToken']
+      @token = res.token if token_reqs.include?(req) and res.respond_to?( :token)
+    end
+  end
+
+  class << self
+    # Your flickr API key, see http://www.flickr.com/services/api/keys for more information
+    attr_accessor :api_key
+
+    # The shared secret of _api_key_, see http://www.flickr.com/services/api/keys for more information
+    attr_accessor :shared_secret
+
+    # Returns the flickr auth URL.
+    def auth_url(args={})
+      full_args = {:api_key => FlickRaw.api_key, :perms => 'read'}
+      args.each {|k, v| full_args[k.to_sym] = v }
+
+      full_args[:api_sig] = api_sig(full_args) if FlickRaw.shared_secret
+
+      url = AUTH_URL + full_args.collect { |a, v| "#{a}=#{v}" }.join('&')
+    end
+
+    # Returns the signature of hsh. This is meant to be passed in the _api_sig_ parameter.
+    def api_sig(hsh)
+      MD5.md5(FlickRaw.shared_secret + hsh.sort{|a, b| a[0].to_s <=> b[0].to_s }.flatten.join).to_s
+    end
+  end
+
+  methods = Flickr.new.call 'flickr.reflection.getMethods'
+  methods.each { |method| Flickr.build_request method.to_s }
+end
+
+class Object
+  # Use this to access the flickr API easily. You can type directly the flickr requests as they are described on the flickr website.
+  #  require 'flickraw'
+  #
+  #  recent_photos = flickr.photos.getRecent
+  #  puts recent_photos[0].title
+  def flickr
+    @flickr ||= FlickRaw::Flickr.new
+  end
+end
+
+FlickRaw.shared_secret = '9c298785e81bc868'
