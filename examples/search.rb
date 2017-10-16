f4c512fb6a33d1453aab20c41574dc6b279e61a4
Andy Duss <mindovermiles262@gmail.com>
2017-10-16 08:49:12 -0700

    add section on api_sig

--- examples/search.rb
+++ examples/search.rb
@@ -1,4 +1,6 @@
 require 'flickraw'
+require 'URI'
+require 'digest/md5'
 
 # search for pictures taken within 60 miles of new brunswick, between 1890-1920
 
@@ -20,3 +22,24 @@ args[:max_taken_date] = '1920-01-01 00:00:00'
 args[:accuracy] = 1 # the default is street only granularity [16], which most images aren't...
 discovered_pictures = flickr.photos.search args
 discovered_pictures.each{|p| url = FlickRaw.url p; puts url}
+
+
+# Example needing api_sig (Searching for safe_search=3)
+# Assumes user has been authenticated per other examples
+
+safe_search_args = {}
+safe_search_args[:auth_token] = token['oauth_token']
+safe_search_args[:api_key] = ENV['FLICKR_API_KEY']
+safe_search_args[:method] = 'flickr.photos.search'
+safe_search_args[:safe_search] = 3  
+
+# Base URL is HTTP Verb + Endpoint URL + all params in alphabetical order
+# We then URL encode Base URL and make an MD5 Digest from encoded URL
+
+base_url = 'POSThttps://api.flickr.com/services/rest/?auth_token=<YOUR TOKEN>&api_key=<YOU KEY>&method=flickr.photos.search&safe_search=3'
+encoded_url = URI.encode_www_form_component(base_url)
+
+safe_search_args[:api_sig] = Digest::MD5.hexdigest(encoded_url)
+
+# Finally, we make the API Call
+unsafe_photos = flickr.photos.search(args)
