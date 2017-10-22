05e9de14b8fd11f1de5a6fb4092c263c928dfb43
Timur Yanberdin <yanberdint@gmail.com>
2017-10-22 04:42:05 +0300

    implement ResponseList#map!

--- lib/flickraw/response.rb
+++ lib/flickraw/response.rb
@@ -38,11 +38,12 @@ class ResponseList < Response
   include Enumerable
   def initialize(h, t, a); super(h, t); @a = a end
   def [](k); k.is_a?(Integer) ? @a[k] : super(k) end
-  def each; @a.each{|e| yield e} end
+  def each; @a.each { |e| yield e } end
   def to_a; @a end
   def inspect; @a.inspect end
   def size; @a.size end
   def marshal_dump; [@h, @flickr_type, @a] end
+  def map!; @a = @a.map { |e| yield e } end
   alias length size
 end
 
