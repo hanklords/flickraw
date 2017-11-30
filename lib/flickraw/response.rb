a6cfd84e1840ca4c917050c1f323d814b702b64d
Yongqiang Qu <netqyq@gmail.com>
2017-11-30 15:45:11 +0800

    fix: wrong method end tag

--- lib/flickraw/response.rb
+++ lib/flickraw/response.rb
@@ -23,7 +23,7 @@ module FlickRaw
                 else v
                 end
         methods << "def #{k}; @h['#{k}'] end;" if Util.safe_for_eval?(k)
-      }
+      end
       eval methods << 'end'
     end
     def [](k); @h[k] end
@@ -38,11 +38,15 @@ module FlickRaw
     include Enumerable
     def initialize(h, t, a); super(h, t); @a = a end
     def [](k); k.is_a?(Integer) ? @a[k] : super(k) end
-    def each; @a.each { |e| yield e } end
+    def each
+       @a.each { |e| yield e }
+    end
     def to_a; @a end
     def inspect; @a.inspect end
     def size; @a.size end
-    def map!; @a = @a.map { |e| yield e } end
+    def map!
+      @a = @a.map { |e| yield e }
+    end
     def marshal_dump; [@h, @flickr_type, @a] end
     alias length size
   end
